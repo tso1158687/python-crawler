459b2e363ea371b7de8d76b196a73fcc8fe3df17
jason <jason.cheng@lileesystems.com>
2020-12-14 00:58:18 +0800

    success to get data

--- crawler.py
+++ crawler.py
@@ -1,28 +1,52 @@
 import requests
+
 from bs4 import BeautifulSoup
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate(
+    "./python-crawler-38a6b-firebase-adminsdk-zs6u9-a33047c4c1.json")
+firebase_admin.initialize_app(cred)
 
-
-def getSingleVideoLink(singleVideoLink: str):
+db = firestore.client()
+def getSingleVideoLink(singleVideoLink: str, videoId: str):
     singlePage = requests.get(singleVideoLink)
     soup = BeautifulSoup(singlePage.text, "html.parser")
-    downloadLinks = soup.find('div', class_="entry-content").find_all('a', href=True)
+    downloadLinks = soup.find(
+        'div', class_="entry-content").find_all('a', href=True)
+    downloadLinkList = []
     for downloadLink in downloadLinks:
+        downloadLinkList.append(downloadLink['href'])
         print(downloadLink['href'])  # 輸出排版後的HTML內容
+    videoData = {
+        'id': videoId,
+        'link': downloadLinkList
+    }
+    # print(videoData)
+    postDataToFirebase(videoData)
 
 
+def postDataToFirebase(videoData):
+    # 初始化firestore
+    
+
+
+    # Add a new doc in collection 'cities' with ID 'LA'
+    db.collection(u'video').document(videoData['id']).set(videoData)
+
+    # video_ref=db.collection("video").document("student")
+    # print(doc_ref.get().to_dict())
+
+
+# main()
 # main
 response = requests.get(
     "https://javfree.me/category/mosaic")
 soup = BeautifulSoup(response.text, "html.parser")
-titles = soup.find_all('a', href=True, class_="thumbnail-link", limit=3)
+titles = soup.find_all('a', href=True, class_="thumbnail-link")
 for res in titles:
-    # print(res['href'])
     print('videoId')
-    # 第二个参数为 1，返回两个参数列表
+
     videoUrlArray = res['href'].split("/")
     print(videoUrlArray[-1])
-    getSingleVideoLink(res['href'])
-    # print(res.prettify())  #輸出排版後的HTML內容
-
-# for a in soup.find_all('a', href=True):
-#     print (a['href'])
+    getSingleVideoLink(res['href'],videoUrlArray[-1])
---
+++ test.py
@@ -0,0 +1,5 @@
+data = {
+    'id': 'migd-619',
+    'link': ['aaa', 'bbb'],
+}
+print(data['id'])
